58db03d4a688c03d58463f833e34a32d2255acad
GreenYun <microsoft.zh.cn@gmail.com>
2023-04-24 12:55:20 +0000

    1682340920

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,8 @@ use std::io::Write;
 
 use log::LevelFilter;
 
+use crate::config::internal::TTYSetColor;
+
 pub fn logging() {
     let level = match std::env::var_os("HKO_BOT_DEBUG").map_or(3, |s| s.to_string_lossy().parse::<u64>().unwrap_or(0)) {
         0 => LevelFilter::Off,
@@ -15,46 +17,28 @@ pub fn logging() {
         _ => LevelFilter::Trace,
     };
 
-    let is_display_time = match std::env::var_os("HKO_BOT_TIME").map(|s| {
+    let is_display_time = std::env::var_os("HKO_BOT_TIME").map_or(false, |s| {
         let s = s.to_string_lossy();
         s.is_empty() || s.to_ascii_lowercase() == "false" || s == "0"
-    }) {
-        Some(true) => true,
-        None | Some(false) => false,
-    };
-
-    let is_journal = !atty::is(atty::Stream::Stdout);
+    });
 
     env_logger::builder()
         .format(move |buf, record| {
             use env_logger::fmt::Color;
             use log::Level;
 
-            if is_journal {
-                let level = 3 * 8
-                    + match record.level() {
-                        Level::Error => 3,
-                        Level::Warn => 4,
-                        Level::Info => 5,
-                        Level::Debug => 6,
-                        Level::Trace => 7,
-                    };
-
-                return writeln!(buf, "<{level}> {}", record.args());
-            }
-
             let mut style = buf.style();
             let timestamp = style
-                .set_color(Color::Rgb(128, 128, 128))
+                .tty_set_color(Color::Rgb(128, 128, 128))
                 .value(format!("[{}]", buf.timestamp()));
 
             let mut style = buf.style();
             let level = match record.level() {
-                Level::Error => style.set_color(Color::Red).value("ERROR"),
-                Level::Warn => style.set_color(Color::Yellow).value("WARN "),
-                Level::Info => style.set_color(Color::Green).value("INFO "),
-                Level::Debug => style.set_color(Color::Blue).value("DEBUG"),
-                Level::Trace => style.set_color(Color::Magenta).value("TRACE"),
+                Level::Error => style.tty_set_color(Color::Red).value("ERROR"),
+                Level::Warn => style.tty_set_color(Color::Yellow).value("WARN "),
+                Level::Info => style.tty_set_color(Color::Green).value("INFO "),
+                Level::Debug => style.tty_set_color(Color::Blue).value("DEBUG"),
+                Level::Trace => style.tty_set_color(Color::Magenta).value("TRACE"),
             };
 
             if is_display_time {
@@ -66,4 +50,22 @@ pub fn logging() {
         .write_style(env_logger::fmt::WriteStyle::Auto)
         .filter_level(level)
         .init();
+}
+
+mod internal {
+    use env_logger::fmt::{Color, Style};
+
+    pub trait TTYSetColor {
+        fn tty_set_color(&mut self, color: Color) -> &mut Self;
+    }
+
+    impl TTYSetColor for Style {
+        fn tty_set_color(&mut self, color: Color) -> &mut Self {
+            if atty::is(atty::Stream::Stdout) {
+                self.set_color(color)
+            } else {
+                self
+            }
+        }
+    }
 }
