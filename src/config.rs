75654b15905d657cadc4d318823d50e5f6159afb
GreenYun <microsoft.zh.cn@gmail.com>
2024-03-02 17:02:10 +0000

    1709398930

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,8 @@
 // Copyright (c) 2022 - 2024 GreenYun Organization
 // SPDX-License-Identifier: MIT
 
+use std::io::Write;
+
 use env_logger::TimestampPrecision;
 use log::LevelFilter;
 
@@ -20,7 +22,30 @@ pub fn logging() {
         .map(|s| s.to_string_lossy().to_ascii_lowercase())
         .is_some_and(|s| !s.is_empty() && !matches!(s.as_str(), "0" | "false" | "no"));
 
-    env_logger::builder()
+    let is_syslog_style = std::env::var_os("HKO_BOT_LOG_STYLE")
+        .map(|s| s.to_string_lossy().to_ascii_lowercase())
+        .is_some_and(|s| s == "syslog");
+
+    let mut builder = env_logger::builder();
+
+    if is_syslog_style {
+        builder.format(|buf, record| {
+            writeln!(
+                buf,
+                "<{}>{}: {}",
+                match record.level() {
+                    log::Level::Error => 3,
+                    log::Level::Warn => 4,
+                    log::Level::Info => 6,
+                    log::Level::Debug | log::Level::Trace => 7,
+                },
+                record.target(),
+                record.args()
+            )
+        });
+    }
+
+    builder
         .format_timestamp(is_display_time.then_some(TimestampPrecision::Seconds))
         .write_style(env_logger::fmt::WriteStyle::Auto)
         .filter_level(level)
