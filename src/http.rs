2fba601d6b99653bee43aa1e2182586099e39051
GreenYun <microsoft.zh.cn@gmail.com>
2026-01-27 18:05:06 +0000

    1769537106

--- src/http.rs
+++ src/http.rs
@@ -1,11 +1,9 @@
-// Copyright (c) 2024 - 2025 GreenYun Organization
+// Copyright (c) 2024 - 2026 GreenYun Organization
 // SPDX-License-Identifier: MIT
 
 use std::sync::OnceLock;
 
 use reqwest::Client;
-use rustls::{ClientConfig, RootCertStore};
-use webpki_roots::TLS_SERVER_ROOTS;
 
 static HTTP_CLIENT: OnceLock<Client> = OnceLock::new();
 
@@ -16,12 +14,7 @@ pub fn client() -> Client {
 fn init_client() -> Client {
 	static USER_AGENT: &str = concat!("curl/8 ", env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
-	let cert_store: RootCertStore = TLS_SERVER_ROOTS.iter().cloned().collect();
-	let mut tls = ClientConfig::builder().with_root_certificates(cert_store).with_no_client_auth();
-	tls.enable_early_data = true;
-	tls.alpn_protocols = vec!["h2".into(), "http/1.1".into()];
-
-	let client = Client::builder().use_preconfigured_tls(tls).user_agent(USER_AGENT).build();
+	let client = Client::builder().user_agent(USER_AGENT).build();
 	match client {
 		Ok(client) => client,
 		Err(e) => {
